6fd72f2c675dd4a0aba32def7168ed1b9c5c0dd6
Dharni <dharnimayekar@gmail.com>
2026-06-23 01:54:37 +0530

    added expenses search and edit modal

--- client/src/components/ExpenseList.jsx
+++ client/src/components/ExpenseList.jsx
@@ -1,4 +1,5 @@
 import API from "../services/api";
+import {useState} from "react";
 
 
 function ExpenseList({
@@ -7,6 +8,11 @@ function ExpenseList({
     selectedCategory,
     setSelectedCategory
  }) {
+
+  const [editingExpense, setEditingExpense] =
+  useState(null);
+    const [searchTerm, setSearchTerm] =
+    useState("");
 
     console.log(expenses[0]);
 console.log(selectedCategory);
@@ -38,6 +44,11 @@ const filteredExpenses =
         (expense) =>
           expense.category === selectedCategory
       );
+const searchedExpenses = filteredExpenses.filter(
+  (expense) => 
+    expense.title.toLowerCase().includes(
+      searchTerm.toLowerCase()));
+    
 
 return (
 <div style={{
@@ -67,6 +78,13 @@ return (
   <option value="bills">Bills</option>
   <option value="entertainment">Entertainment</option>
 </select>
+  <input 
+type="text" 
+placeholder="Search Expense..." 
+value= {searchTerm}
+onChange={(e) => setSearchTerm(e.target.value)}
+style ={{ padding: "10px", marginLeft: "10px", borderRadius: "6px" }}/>  
+
 
 <table style={{
     width:"100%",
@@ -82,12 +100,13 @@ return (
           <th style={{ padding: "12px" }}>Category</th>
           <th style={{ padding: "12px" }}>Amount</th>
           <th style={{ padding: "12px" }}>Date</th>
-          <th style={{ padding: "12px" }}>Action</th>            
+          <th style={{ padding: "12px" }}>Edit</th>
+          <th style={{ padding: "12px" }}>Delete</th>            
         </tr>
     </thead>
     <tbody>
         {
-            filteredExpenses.map((expense) => (
+            searchedExpenses.map((expense) => (
                 <tr key = { expense._id}
                 style={{
                     textAlign: "center",
@@ -114,6 +133,22 @@ return (
 <td style={{ padding: "12px" }}>
   {new Date(expense.createdAt).toLocaleDateString()}
 </td>
+<td>
+  <button
+  onClick={() => setEditingExpense(expense)}
+  style={{
+    background: "#F59E0B",
+    color: "white",
+    border: "none",
+    padding: "8px 14px",
+    borderRadius: "6px",
+    cursor: "pointer",
+    marginRight: "8px"
+  }}
+>
+  Edit
+</button>
+</td>
 
             <td style={{ padding: "12px" }}>
               <button
@@ -137,7 +172,69 @@ return (
         }
     </tbody>
 </table>
+{
+  editingExpense && (
+    <div
+      style={{
+        position: "fixed",
+        top: "20%",
+        left: "35%",
+        background: "white",
+        padding: "20px",
+        borderRadius: "10px",
+        boxShadow: "0 0 10px gray"
+      }}
+    >
+      <h3>Edit Expense</h3>
 
+      <input
+        type="text"
+        value={editingExpense.title}
+        onChange={(e) =>
+          setEditingExpense({
+            ...editingExpense,
+            title: e.target.value
+          })
+        }
+        style={{
+    width: "100%",
+    padding: "10px"
+  }}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        value={editingExpense.amount}
+        onChange={(e) =>
+          setEditingExpense({
+            ...editingExpense,
+            amount: e.target.value
+          })
+        }
+        style={{
+    width: "100%",
+    padding: "10px"
+  }}
+      />
+
+      <br /><br />
+
+      <button>
+        Save
+      </button>
+
+      <button
+        onClick={() =>
+          setEditingExpense(null)
+        }
+      >
+        Cancel
+      </button>
+    </div>
+  )
+}
 </div>
 );
 }
