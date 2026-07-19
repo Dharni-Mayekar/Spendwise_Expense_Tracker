3d8b49dbabbae6a56cdf415af0c3f3c5993310f5
Dharni <dharnimayekar@gmail.com>
2026-07-20 00:58:21 +0530

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -123,13 +123,19 @@ const forgotPassword = async (req, res) => {
     await user.save();
     console.log("5. User saved");
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+// Email Transport
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
     console.log("6. Transporter created");
 
