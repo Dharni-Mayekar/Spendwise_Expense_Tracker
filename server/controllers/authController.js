bcad362e92290e2d40d9efc5daab88bde2afcde3
Dharni <dharnimayekar@gmail.com>
2026-07-20 00:47:01 +0530

    Fix reset password URL

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -100,9 +100,13 @@ message: error.message,
 
 const forgotPassword = async (req, res) => {
   try {
+    console.log("1. Forgot password started");
+
     const { email } = req.body;
+    console.log("2. Email:", email);
 
     const user = await User.findOne({ email });
+    console.log("3. User found:", !!user);
 
     if (!user) {
       return res.status(404).json({
@@ -110,15 +114,15 @@ const forgotPassword = async (req, res) => {
       });
     }
 
-    // Generate Token
     const resetToken = crypto.randomBytes(32).toString("hex");
+    console.log("4. Token generated");
 
     user.resetPasswordToken = resetToken;
-    user.resetPasswordExpire = Date.now() + 5 * 60 * 1000; // 15 min
+    user.resetPasswordExpire = Date.now() + 5 * 60 * 1000;
 
     await user.save();
+    console.log("5. User saved");
 
-    // Email Transport
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -127,7 +131,13 @@ const forgotPassword = async (req, res) => {
       },
     });
 
-    const resetURL = `http://localhost:5173/reset-password/${resetToken}`;
+    console.log("6. Transporter created");
+
+const resetURL =
+  process.env.NODE_ENV === "production"
+    ? `https://spendwise-expense-tracker-wine.vercel.app/reset-password/${resetToken}`
+    : `http://localhost:5173/reset-password/${resetToken}`;
+    console.log("7. Sending mail...");
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
@@ -135,21 +145,18 @@ const forgotPassword = async (req, res) => {
       subject: "SpendWise Password Reset",
       html: `
         <h2>Password Reset</h2>
-        <p>Click the link below to reset your password.</p>
-
-        <a href="${resetURL}">
-          Reset Password
-        </a>
-
-        <p>This link expires in 5 minutes.</p>
+        <a href="${resetURL}">Reset Password</a>
       `,
     });
+
+    console.log("8. Mail sent");
 
     res.json({
       message: "Reset email sent successfully.",
     });
 
   } catch (error) {
+    console.log("ERROR:", error);
     res.status(500).json({
       message: error.message,
     });
